628df7a110710b472e286b1b2a8cacbb9acaf4f0
nachatz <nik.achatz@gmail.com>
2023-07-23 23:47:13 -0700

    revert: temp logo

--- client/src/components/Navbar/NavItems/NavItems.jsx
+++ client/src/components/Navbar/NavItems/NavItems.jsx
@@ -7,8 +7,8 @@ export default function NavItems({ navigation }) {
   return (
     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
       <div className="flex flex-shrink-0 items-center">
-        <img className="block h-15 w-20 lg:hidden" src="" alt="" />
-        <img className="hidden h-15 w-20  lg:block" src="" alt="" />
+        <img className="block h-10 w-10 lg:hidden" src="https://media.licdn.com/dms/image/C5603AQFbUePI_2E0Cg/profile-displayphoto-shrink_400_400/0/1616816921606?e=1695859200&v=beta&t=7ZAXnYgPeT3tTLk39Bs0NBAEbnyc2B7oL4cPx8K45TU" alt="" />
+        <img className="hidden h-10 w-10  lg:block" src="https://media.licdn.com/dms/image/C5603AQFbUePI_2E0Cg/profile-displayphoto-shrink_400_400/0/1616816921606?e=1695859200&v=beta&t=7ZAXnYgPeT3tTLk39Bs0NBAEbnyc2B7oL4cPx8K45TU" alt="" />
       </div>
       <div className="hidden sm:ml-6 sm:block">
         <div className="flex items-center justify-center space-x-4 h-full">
